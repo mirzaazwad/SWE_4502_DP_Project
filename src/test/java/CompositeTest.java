62e6b82b406b1e6b5017373d4976ad5f264c93fd
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-11-19 18:33:27 +0600

    Composite Test Fix

--- src/test/java/CompositeTest.java
+++ src/test/java/CompositeTest.java
@@ -10,20 +10,21 @@ import java.util.List;
 public class CompositeTest {
     private Flock flock;
     private List<String> duck_names;
+
     @BeforeEach
-    public void init(){
-        flock=new Flock();
-        this.duck_names=new ArrayList<String>();
+    public void init() {
+        flock = new Flock();
+        this.duck_names = new ArrayList<String>();
         this.duck_names.add("Donald");
         this.duck_names.add("Ronald");
         this.duck_names.add("Vivi");
         this.duck_names.add("Ducky");
         this.duck_names.add("Bad Ducky");
-        IQuackable mallard=new MallardDuck(duck_names.get(0));
-        IQuackable marbled=new MarbledDuck(duck_names.get(1));
-        IQuackable alabian=new AlabioDuck(duck_names.get(2));
-        IQuackable rubber=new RubberDuck(duck_names.get(3));
-        IQuackable wooden=new WoodenDuck(duck_names.get(4));
+        IQuackable mallard = new MallardDuck(duck_names.get(0));
+        IQuackable marbled = new MarbledDuck(duck_names.get(1));
+        IQuackable alabian = new AlabioDuck(duck_names.get(2));
+        IQuackable rubber = new RubberDuck(duck_names.get(3));
+        IQuackable wooden = new WoodenDuck(duck_names.get(4));
         flock.add(mallard);
         flock.add(marbled);
         flock.add(alabian);
@@ -32,21 +33,22 @@ public class CompositeTest {
     }
 
     @Test
-    public void TestQuack(){
+    public void TestQuack() {
         assertEquals("Quack\n" +
                 "Quack\n" +
                 "Quack\n" +
-                "Squeak\n\n",flock.quack());
+                "Squeak\n\n", flock.quack());
     }
 
     @Test
-    public void TestGetName(){
-        String expected="";
-        for (String dummy_duck:
-             duck_names) {
-            expected+=dummy_duck+"\n";
+    public void TestGetName() {
+        String expected = "";
+        Integer i = 1;
+        for (String dummy_duck : duck_names) {
+            expected += i.toString() + ". " + dummy_duck + "\n";
+            i += 1;
         }
-        assertEquals(expected,flock.getName());
+        assertEquals(expected, flock.getName());
     }
 
 }
